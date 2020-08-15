ecc3aecda291ba847cc4ec264c0935f7365e0647
unknown <brightbeamgames@gmail.com>
2020-08-15 02:50:57 -0400

    update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ WIDTH = 360
 HEIGHT = 480
 FPS = 30
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
 # Initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
@@ -15,7 +21,18 @@ clock = pygame.time.Clock()
 # Game Loop
 running = True
 while running:
+    # Keep loop running at the right speed
+    clock.tick(FPS)
     #Process input
+    for event in pygame.event.get():
+        # check for closing the window
+        if event.type == pygame.QUIT:
+            running = False
     #Update
+
     #Draw / render
-    screen.fill()
+    screen.fill(BLACK)
+    # *after* drawing everything
+    pygame.display.flip()
+
+pygame.quit()
